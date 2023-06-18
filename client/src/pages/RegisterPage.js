d8e199bf8be13a60a026b45ae549dbe51e324b19
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-06-18 10:22:41 +0300

    login-page-done-v1

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const RegisterPage = () => {
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+const register = async (e) => {
+    e.preventDefault();
+    /* We didn't used axios for fetch from backend */
+    const response = await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type':'application/json'},
+    });
+    if (response.status === 200) {
+      alert('registration successful');
+    } else {
+      alert('registration failed');
+    }
+}
+
   return (
-    <div>RegisterPage</div>
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input type="text"
+             placeholder="Username"
+             value={username}
+             onChange={e => setUsername(e.target.value)}/>
+      <input type="password"
+             placeholder="Password"
+             value={password}
+             onChange={e => setPassword(e.target.value)}/>
+      <button>Register</button>
+    </form>
   )
 }
---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const app = express();
+
+/* It generates bcrypt password according to this string */
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfasjdf3423sf';
+/* MIDDLEWARE */
+/* Cors Library help get post request withour CORS error from website */
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://uygurberkay0:123456Asd@cluster0.afusyu3.mongodb.net/')
+
+
+/* Register Post Request */
+app.post('/register', async (req,res) => {
+    const {username,password} = req.body;
+    try{
+      const userDoc = await User.create({
+        username,
+        password:bcrypt.hashSync(password,salt),
+      });
+      res.json(userDoc);
+    } catch(e) {
+      console.log(e);
+      res.status(400).json(e);
+    }
+  });
+
+  
+/* Login Post Request */
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);s
+    if (passOk) {
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  });
+
+
+app.listen(4000, ()=> {
+    console.log(`Server is running at port : ${4000}`)
+});
